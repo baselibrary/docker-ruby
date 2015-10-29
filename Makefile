02b88691c2bd7801ceffec06d140f795a82eb304
Shawn Ma <qsma@thoughtworks.com>
2015-10-29 17:35:35 +0800

    refactory

--- Makefile
+++ Makefile
@@ -1,20 +1,27 @@
-NAME = quay.io/baselibrary/ruby
-REPO = git@github.com:baselibrary/docker-ruby.git
-TAGS = 1.8 1.9 2.0 2.1 2.2
+NAME     = baselibrary/ruby
+REPO     = git@github.com:baselibrary/docker-ruby.git
+VERSIONS = $(foreach df,$(wildcard */Dockerfile),$(df:%/Dockerfile=%))
 
 all: build
 
-build: $(TAGS)
+build: $(VERSIONS)
 
-release: $(TAGS)
+release: $(VERSIONS)
 	docker push ${NAME}
+
+update:
+	docker run --rm -v $$(pwd):/work -w /work buildpack-deps ./update.sh
+
+library:
+	docker run --rm -v $$(pwd):/work -w /work buildpack-deps ./generate-stackbrew-library.sh
 
 sync-branches:
 	git fetch $(REPO) master
-	@$(foreach tag, $(TAGS), git branch -f $(tag) FETCH_HEAD;)
-	@$(foreach tag, $(TAGS), git push $(REPO) $(tag);)
-	@$(foreach tag, $(TAGS), git branch -D $(tag);)
+	@$(foreach tag, $(VERSIONS), git branch -f $(tag) FETCH_HEAD;)
+	@$(foreach tag, $(VERSIONS), git push $(REPO) $(tag);)
+	@$(foreach tag, $(VERSIONS), git branch -D $(tag);)
 
-.PHONY: $(TAGS)
-$(TAGS):
-	docker build -t $(NAME):$@ $@
+
+.PHONY: all build library $(VERSIONS)
+$(VERSIONS):
+	docker build --rm -t $(NAME):$@ $@
---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+
+for version in "${versions[@]}"; do	
+  if [ "${version}" == "1.8" ]; then
+    package="ruby${version}"
+    rubygems="rubygems1.8"
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+  elif [ "${version}" == "1.9" ]; then
+  	package="ruby1.9.1"
+  	rubygems=""
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+  else
+  	package="ruby${version}"
+  	rubygems=""
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+	fi
+	(
+		set -x
+		sed '
+			s/%%RUBY_MAJOR%%/'"$version"'/g;
+			s/%%RUBY_VERSION%%/'"$fullVersion"'/g;
+			s!%%RUBY_PACKAGE%%!'"$package"'!g;
+      s!%%RUBY_RUBYGEMS%%!'"$rubygems"'!g;
+		' Dockerfile.template > "$version/Dockerfile"
+	)
+done
+
