25a3f7c39fb3e27022161711b2495dc3962d6603
Shawn Ma <qsma@thoughtworks.com>
2015-11-06 09:22:15 +0800

    update

--- update.sh
+++ update.sh
@@ -12,25 +12,25 @@ versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do	
   if [ "${version}" == "1.8" ]; then
-    package="ruby${version}"
-    rubygems="rubygems1.8"
-    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+    fullGems="rubygems1.8"
+    fullPackage="ruby1.8"
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$fullPackage -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
   elif [ "${version}" == "1.9" ]; then
-  	package="ruby1.9.1"
-  	rubygems=""
-    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+    fullGems=""
+  	fullPackage="ruby1.9.1"
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$fullPackage -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
   else
-  	package="ruby${version}"
-  	rubygems=""
-    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$package -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
+    fullGems=""
+  	fullPackage="ruby${version}"
+    fullVersion="$(curl -fsSL "http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname=$fullPackage -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | sort -rV | head -n1 )"
 	fi
 	(
 		set -x
 		sed '
-			s/%%MAJOR%%/'"$version"'/g;
-			s/%%VERSION%%/'"$fullVersion"'/g;
-			s!%%PACKAGE%%!'"$package"'!g;
-      s!%%RUBYGEMS%%!'"$rubygems"'!g;
+			s/%%RUBY_MAJOR%%/'"$version"'/g;
+			s/%%RUBY_VERSION%%/'"$fullVersion"'/g;
+			s!%%RUBY_PACKAGE%%!'"$fullPackage"'!g;
+      s!%%RUBY_GEMS%%!'"$fullGems"'!g;
 		' Dockerfile.template > "$version/Dockerfile"
 	)
 done
